6cbe430cb15b016f058fae4a604223984eae98cc
Jeremy Cunningham <jpcunningh@gmail.com>
2019-01-14 22:43:28 -0600

    Fix eslint findings

--- fillEntries.js
+++ fillEntries.js
@@ -28,30 +28,31 @@ const sgv = {
   g5calibrated: false,
 };
 
-let time = 0;
+let entryTime = 0;
 let numEntries = 0;
 
 Debug.enable('fillEntries:*,xDripAPS:*');
 
 if (argv.argv._[0]) {
-  time = moment(argv.argv._[0]);
+  entryTime = moment(argv.argv._[0]);
 } else {
   error('Must provide an entry time to use');
   process.exit(1);
 }
 
 if (argv.argv._[1]) {
-  numEntries = argv.argv._[1];
+  [, numEntries] = argv.argv._;
 } else {
   error('Must provide the number of entries to upload');
   process.exit(1);
 }
 
-const fillEntries = async (entryTime, numEntries) => {
+const fillEntries = async () => {
   for (; numEntries > 0; numEntries -= 1) {
     sgv.readDate = entryTime.valueOf();
     sgv.readDateMills = entryTime.valueOf();
 
+    /* eslint-disable-next-line no-await-in-loop */
     await snooze(10);
 
     xDripAPS.post(sgv, false);
@@ -60,5 +61,4 @@ const fillEntries = async (entryTime, numEntries) => {
   }
 };
 
-fillEntries(time, numEntries);
-
+fillEntries();
